a3b7ba9770398d6d3485cfaf707e740463635494
MonicaValenciaCode <monicacoding@outlook.com>
2025-09-26 15:56:43 +0100

    dev-monicavo add unit test config and pipeline

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+// jest.config.js
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
